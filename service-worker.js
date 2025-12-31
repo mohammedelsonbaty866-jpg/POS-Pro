62a17cadca2dd15587a93aece8a4226c2d21b22c
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-31 16:41:30 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,14 @@
-const CACHE_NAME = "accounting-pwa-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
+self.addEventListener('install',e=>{
+ e.waitUntil(
+  caches.open('pospro').then(c=>c.addAll([
+   'index.html',
+   'landing.html'
+  ]))
+ );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch',e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+ );
 });
